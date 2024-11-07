25db2ad5b0705e7c0f4068428ba785696952fc2e
sofiiaru <rudenka93@gmail.com>
2024-11-07 16:06:10 -0500

    twosum

--- java-dev/src/main/java/data_structures/TwoSum.java
+++ java-dev/src/main/java/data_structures/TwoSum.java
@@ -1,2 +1,18 @@
-package data_structures;public class TwoSum {
+package data_structures;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    //brute force solution - nested loop and cehcl all combinations
+    public static int[] twoSum(int[] arr, int target){
+        for(int i = 0; i< arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+
+            }
+        }
+        return new int[]{0,0};
+
+    }
 }
