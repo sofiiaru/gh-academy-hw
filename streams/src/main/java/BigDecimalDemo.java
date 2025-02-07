d40c137417a95b587829b09317cc15821e1e983f
sofiiaru <rudenka93@gmail.com>
2025-02-07 10:35:23 -0500

    DM HW Week 16

--- streams/src/main/java/BigDecimalDemo.java
+++ streams/src/main/java/BigDecimalDemo.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BigDecimalDemo {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigDecimalDemo {
+
+    public static void main(String[] args) {
+
+        double d1 = 174.56;
+        double d2 = 174.26;
+
+        System.out.println(d1-d2);
+
+        BigDecimal b1 = new BigDecimal("174.56");
+        BigDecimal b2 = new BigDecimal("174.26");
+
+        System.out.println(b1.subtract(b2));
+
+        //scaling and rounding
+        BigDecimal number1 = new BigDecimal("23.12");
+        System.out.println(number1.setScale(1, RoundingMode.HALF_DOWN));
+        System.out.println(number1.compareTo(new BigDecimal("23.12")));
+
+        //BigDecimals are immutable
+
+    }
 }
--- streams/src/main/java/OptionalDemo.java
+++ streams/src/main/java/OptionalDemo.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class OptionalDemo {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalDemo {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(2,4,6,3,2,23);
+
+        //empty() - isPresent()
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty.isPresent());
+
+        System.out.println(Optional.of(numbers).isPresent());
+
+        //ifPresent()
+       Optional<Integer> bigNum = numbers.stream().filter(x -> x>8).findAny();
+
+       //get
+        System.out.println(bigNum.get());
+
+        //orElse()
+        System.out.println(bigNum.orElse(0));
+
+    }
 }
